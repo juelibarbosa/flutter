c78bb52f475d4389ccf767cf9c88b28c6a3a9739
Kien Pham <kien@sendgrid.com>
2012-08-11 13:33:06 -0700

    adding post file

---
+++ post.php
@@ -0,0 +1,9 @@
+<?php
+
+$response = '<?xml version="1.0" encoding="UTF-8"?>
+<Response>
+    <Say voice="woman">Please leave a message after the tone.</Say>
+    <Record maxLength="20" />
+</Response>';
+
+echo $response;
