4c5116d818df7b0cd05739eb30e4023c02d751c3
Kien Pham <kien@sendgrid.com>
2012-08-11 13:49:27 -0700

    logging

--- post.php
+++ post.php
@@ -2,8 +2,11 @@
 
 $response = '<?xml version="1.0" encoding="UTF-8"?>
 <Response>
-    <Say voice="woman">Please leave a message after the tone.</Say>
+    <Say voice="woman">Say what you want to say</Say>
     <Record maxLength="20" />
 </Response>';
 
 echo $response;
+
+// save post to file:
+file_put_contents(print_r($_POST, true), 'log.txt');
