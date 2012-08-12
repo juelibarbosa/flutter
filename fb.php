5770d8fc6a489981197a2ed39b472c2bb98cbee2
Kien Pham <kien@sendgrid.com>
2012-08-12 10:49:18 -0700

    debug

--- fb.php
+++ fb.php
@@ -9,7 +9,7 @@ if (isset($_GET['code']))
 	{
 		// get the user access token:
 		$url = "https://graph.facebook.com/oauth/access_token?client_id=354211487989261&client_secret=3f71e62611254ff4fe09210955295686&redirect_uri=http://flutter.phpfogapp.com/fb.php&code=$code";
-
+echo $url;
 		$token = file_get_contents($url);
 var_dump($token);		
 	    $params = null;
