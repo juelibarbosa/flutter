7ce3fdd36bbb4bf65ae88c3f3e6734cfde6271f8
Kien Pham <kien@sendgrid.com>
2012-08-12 11:02:30 -0700

    debug

--- fb.php
+++ fb.php
@@ -10,14 +10,13 @@ if (isset($_GET['code']))
 		// get the user access token:
 		$url = "https://graph.facebook.com/oauth/access_token?client_id=354211487989261&client_secret=3f71e62611254ff4fe09210955295686&redirect_uri=http://flutter.phpfogapp.com/fb.php&code=$code";
 
-		$ch = curl_init();
-
 		// Set query data here with the URL
+		$ch = curl_init();
 		curl_setopt($ch, CURLOPT_URL, $url); 
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 		curl_setopt($ch, CURLOPT_TIMEOUT, '3');
 		$token = trim(curl_exec($ch));
-		curl_close($ch);
+		
 
 		$params = null;
 		//$token = file_get_contents($url);
@@ -25,7 +24,11 @@ if (isset($_GET['code']))
 var_dump($params);
      	$graph_url = "https://graph.facebook.com/me?access_token=" . $params['access_token'];
 
-     	$user = json_decode(file_get_contents($graph_url));
+     	//$user = json_decode(file_get_contents($graph_url));
+     	curl_setopt($ch, CURLOPT_URL, $graph_url);
+     	$user = json_decode(trim(curl_exec($ch)));
+		curl_close($ch);
+
 var_dump($user);exit;     	
      	// save all the auth stuff to session:
      	$_SESSION['auth'] = array(
