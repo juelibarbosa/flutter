125284b90a04dfe21a9a4aa99d2047d2877839b2
Kien Pham <kien@sendgrid.com>
2012-08-12 10:54:49 -0700

    debug

--- fb.php
+++ fb.php
@@ -9,7 +9,17 @@ if (isset($_GET['code']))
 	{
 		// get the user access token:
 		$url = "https://graph.facebook.com/oauth/access_token?client_id=354211487989261&client_secret=3f71e62611254ff4fe09210955295686&redirect_uri=http://flutter.phpfogapp.com/fb.php&code=$code";
-echo $url;
+
+		$ch = curl_init();
+
+		// Set query data here with the URL
+		curl_setopt($ch, CURLOPT_URL, $url); 
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_TIMEOUT, '3');
+		$content = trim(curl_exec($ch));
+		curl_close($ch);
+print $content . "\n\n";
+
 		$token = file_get_contents($url);
 var_dump($token);		
 	    $params = null;
