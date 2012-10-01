1b338a431db3d2bad8e36a4192eaa27e6ff0dca7
Kien Pham <kien@sendgrid.com>
2012-10-01 10:23:39 -0700

    remove secret config

--- config.php
+++ config.php
@@ -1,25 +1,2 @@
 <?php
 session_start();
-
-// facebook:
-$fbAPIKey = '354211487989261';
-$fbSecret = '3f71e62611254ff4fe09210955295686';
-$fb_redirect_url = 'http://flutter.phpfogapp.com';
-//$fb_redirect_url = 'http://localhost:8080';
-
-// twilio:
-$ApiVersion = "2010-04-01";
-// Kien twilio
-//$account_id = 'AC37440124450021dbf4a74d71a1d9c467';
-//$auth_token = 'e157f60d0c4a744e238dfa0aec525d58';
-//$twilioAppId = 'APfc0ca6064feb95838cccee0137c4cb62';
-
-// Conrad twilio:
-$accountSid = 'AC701ddef9bce91404467507cd870034db';
-$authToken  = '1bd60ff00f687b84eea8408be49f6a71';
-$baseAPIUrl = 'https://api.twilio.com';
-$twilioAppId = 'AP6ea1770e590447ce5081b6c8ab18d818';
-
-// domain, godaddy
-// u: flutterus
-// p: flutter810
